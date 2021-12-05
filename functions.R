b38e829a3befe60f3a1f12ce2523cf9c0731eb00
Michele Russo <russomichele1998@gmail.com>
2021-12-05 12:18:52 +0100

    Update functions.R
    Added a function to compute the cliques and the separators of an undirected graph.

--- functions.R
+++ functions.R
@@ -63,3 +63,54 @@ plotGraph = function(adjacencyMatrix, variables.names = NULL){
   x11()
   plot(graph)
 }
+
+# Computes the (maximal) cliques and (minimal) separators of the graph. adjacencyMatrix
+# is a matrix representing the adjacency matrix of an undirected graph. value is a list
+# of two lists: the first one is a list of the cliques and the second one is a list of
+# the separators. Optional: variables.names is a vector
+# of strings representing the names of the nodes of the graph.
+getCliquesAndSeparators = function(adjacencyMatrix,variables.names = NULL){
+  if(!isUndirectedGraph(adjacencyMatrix)){
+    stop("Adjacency matrix does not represent an undirected graph.")
+  }
+  if(!is.null(variables.names)){
+    if(length(variables.names) != dim(adjacencyMatrix)[1]){
+      stop("Length of variables.names is not correct.")
+    }
+    colnames(adjacencyMatrix) = rownames(adjacencyMatrix) = variables.names
+  }
+  graph = graph_from_adjacency_matrix(adjacencyMatrix, mode = "undirected")
+  cliques = maximal.cliques(graph)
+  separators = min_separators(graph)
+  cliques.list = list()
+  for(i in 1:length(cliques)){
+    clique = cliques[[i]]
+    clique = colnames(adjacencyMatrix)[as.vector(clique)]
+    cliques.list[[i]] = clique
+  }
+  separators.list = list()
+  for(i in 1:length(separators)){
+    separator = separators[[i]]
+    separator = colnames(adjacencyMatrix)[as.vector(separator)]
+    separators.list[[i]] = separator
+  }
+  return(list(cliques.list,separators.list))
+}
+
+# Computes the adjacency matrix of an undirected graph given a vector representing
+# its edges. In particualar, edges is a vector such that the first edge is between
+# the first and second element of edges, the second edge is between the third and the
+# fourth element of edges and so on. Optional: variables.names is a vector
+# of strings representing the names of the nodes of the graph.
+getAdjacencyMatrixFromEdges = function(edges, variables.names = NULL){
+  graph = make_graph(edges, directed = FALSE)
+  adjacencyMatrix = as_adjacency_matrix(graph, sparse = 0)
+  if(!is.null(variables.names)){
+    if(length(variables.names) != dim(adjacencyMatrix)[1]){
+      stop("Length of variables.names is not correct.")
+    }
+    colnames(adjacencyMatrix) = rownames(adjacencyMatrix) = variables.names
+  }
+  return(adjacencyMatrix)
+}
+
