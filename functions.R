4e66a6f9c0eb7748d0430a2f0b034914c4c5fa6c
Michele Russo <russomichele1998@gmail.com>
2021-12-05 11:13:43 +0100

    Create functions.R
    Implemented functions to:
    - Generate a dataset of categorical random variables (just for testing, not final version as cannot encode the conditional dependecies)
    - Compute the counts of occurencies for a dataset of categorical random variables.
    - Check whether a matrix represents the adjacency matrix of an undirected graph.
    - Plot a graph given its adjacency matrix.

---
+++ functions.R
@@ -0,0 +1,65 @@
+library(extraDistr)
+library(dplyr)
+library(igraph)
+library(matrixcalc)
+
+# n.obs is the number of observations to simulate (int), variables.names is a vector of 
+# strings correpsonding to the names of the variables to simulate and variables.values
+# is a list of vectors corresponding to the possible values of the variables (therefore
+# the length of variables.values has to be the same of the length of variables.names).
+generateCategoricalData = function(n.obs, variables.names, variables.values){
+  data = data.frame(matrix(ncol = length(variables.names), nrow = n.obs))
+  for(i in 1:length(variables.names)){
+    values = variables.values[[i]]
+    column = values[rdunif(n.obs,1,length(values))]
+    data[,i] = column
+  }
+  colnames(data) = variables.names
+  return(data)
+}
+
+# data is a dataframe of categorical variables, variables.names is a vector of strings
+# corresponding to the names of the variables to consider and variables.values is a 
+# vector whose elements corresponds to the values of the variables in variables.names.
+computeCounts = function(data, variables.values, variables.names = NULL){
+  if(is.null(variables.names)){
+    variables.names = colnames(data)
+  }
+  if(length(variables.names) != length(variables.values)){
+    stop("The lenghts of variables.names and variables.values do not match")
+  }
+  for(i in 1:length(variables.names)){
+    data = data %>% filter(data[variables.names[i]] == variables.values[i])
+  }
+  return(dim(data)[1])
+}
+
+# Checks if adjacencyMatrix is the adjacency matrix of an undirected graph. 
+isUndirectedGraph = function(adjacencyMatrix){
+  if(!is.square.matrix(adjacencyMatrix)){
+    message("Matrix is not the adjacency matrix of a graph as it is not square.")
+    return(FALSE)
+  }
+  if(!isSymmetric(adjacencyMatrix)){
+    message("Graph is not undirected as the adjacency matrix is not symmetric.")
+    return(FALSE)
+  }
+  return(TRUE)
+}
+
+# Plots the graph given its adjacency matrix. Optional: variables.names is a vector
+# of strings representing the names of the nodes of the graph.
+plotGraph = function(adjacencyMatrix, variables.names = NULL){
+  if(!isUndirectedGraph(adjacencyMatrix)){
+    stop("Adjacency matrix does not represent an undirected graph.")
+  }
+  if(!is.null(variables.names)){
+    if(length(variables.names) != dim(adjacencyMatrix)[1]){
+      stop("Length of variables.names is not correct.")
+    }
+    colnames(adjacencyMatrix) = rownames(adjacencyMatrix) = variables.names
+  }
+  graph = graph_from_adjacency_matrix(adjacencyMatrix, mode = "undirected")
+  x11()
+  plot(graph)
+}
